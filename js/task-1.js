3ecff716c8fa95fe980654330e2515fb972da68c
evgen_zoidze <1ovva.ungrace@gmail.com>
2024-11-03 21:08:00 +0100

    HW2

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,7 @@
 function makeTransaction(quantity, pricePerDroid) {
   const totalPrice = quantity * pricePerDroid;
-  console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+  const message = `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  return message;
 }
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
